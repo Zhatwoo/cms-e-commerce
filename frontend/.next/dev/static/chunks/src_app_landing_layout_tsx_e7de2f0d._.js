d59d436ebdfc04ef4d9240e7da889c7ffda9f4e4
Zhatwoo <111881703+Zhatwoo@users.noreply.github.com>
2026-02-03 12:58:45 +0800

    Delete frontend/.next directory

--- frontend/.next/dev/static/chunks/src_app_landing_layout_tsx_e7de2f0d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_landing_components_header_tsx_50730c91._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/src_app_landing_page_tsx_e7de2f0d._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_landing_components_Globe3D_tsx_7829f0d1._.js",
-  "static/chunks/src_app_landing_components_527bc7bc._.js",
-  "static/chunks/node_modules_next_dist_shared_lib_d43e9fa9._.js"
-],
-    source: "dynamic"
-});
